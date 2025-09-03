db7aced1bbec0cae8941afcca4d414fb8383f24f
Srimannarayana <87487895+Srimannarayana2021@users.noreply.github.com>
2025-09-03 09:54:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 //your JS code here. If required.
 
-document.addEventListener("DOMcontentLoaded" ,(e)=>{
-	console.log("DOM load success");
-})
+document.addEventListener("DOMContentLoaded" ,(e)=>{
+	// console.log("DOM load success");
+	const msg = document.createElement("div");
+	msg.textContent = "DOM load success";
+	document.body.appendChild(msg);
+});
